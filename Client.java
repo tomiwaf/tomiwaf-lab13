6e8404286d02325d89e787d4b009888ab5be6ded
tomiwaf <o.fasosin@gwu.edu>
2026-04-13 16:19:51 -0400

    made some instance variables and the parameterized constructor

---
+++ Client.java
@@ -0,0 +1,12 @@
+public class Client {
+
+    private String name;
+    private int port;
+    public Client(String name, int port) {
+        this.name = name;
+        this.port = port;
+    }
+    public Socket getSocket() {
+
+    }
+}
